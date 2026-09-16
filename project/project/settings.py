b670f5a0a51e4921db80a7917510951425bf8cb7
shreyaunni1302 <shreyaunni1302@gmail.com>
2026-09-16 18:22:00 +0530

    updated portfolio

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-77&25)!g!1+mas77@f(*b8o6k84(tsqo-#4&2vm%fhqkudmlyu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-w7be.onrender.com']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "portfolio-w7be.onrender.com"]
 
 
 # Application definition
